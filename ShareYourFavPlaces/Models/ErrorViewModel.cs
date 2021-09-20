a8bb1450842113a75bc618bfd3e35d0ad0add39f
HuidanKuang <77803403+HuidanKuang@users.noreply.github.com>
2021-09-19 23:07:04 -0400

    Add project files.

---
+++ ShareYourFavPlaces/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShareYourFavPlaces.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
